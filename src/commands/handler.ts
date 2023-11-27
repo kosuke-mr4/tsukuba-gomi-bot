a0172564fc61433ca3ab176510d3d74ca77accbf
kosuke-mr4 <s1911381@coins.tsukuba.ac.jp>
2023-11-27 19:40:31 +0900

    add : showarea

--- src/commands/handler.ts
+++ src/commands/handler.ts
@@ -3,11 +3,13 @@ import { COMMANDS } from "../constants/commands";
 import { timeCommand } from "./time";
 import { helpCommand } from "./help";
 import { setupCommand } from "./setup";
+import { showareaCommand } from "./showarea";
 
 const commands = new Map();
 
 commands.set(COMMANDS.TIME, timeCommand);
 commands.set(COMMANDS.HELP, helpCommand);
 commands.set(COMMANDS.SETUP, setupCommand);
+commands.set(COMMANDS.SHOWAREA, showareaCommand);
 
 export { commands };
---
+++ src/commands/showarea.ts
@@ -0,0 +1,28 @@
+import { Message } from "discord.js";
+import fs from "fs";
+
+const { parse } = require("csv-parse/sync");
+
+export const showareaCommand = async (message: Message) => {
+  const date = new Date();
+  const year = date.getFullYear();
+  const month = date.getMonth();
+  const fileName = `src/data/${year}${month}.csv`;
+
+  fs.access(fileName, fs.constants.F_OK, (err) => {
+    if (err) {
+      message.channel.send(
+        "ファイルが存在しません。!gomi setup コマンドを実行してください。"
+      );
+    } else {
+      const data = fs.readFileSync(fileName, "utf8");
+      const rows = parse(data, { columns: true });
+      const firstLineArray = rows.map((row: Record<string, string>) => {
+        return row[Object.keys(row)[0]];
+      });
+
+      const targetmessage = firstLineArray.join(" , ");
+      message.channel.send(targetmessage);
+    }
+  });
+};
